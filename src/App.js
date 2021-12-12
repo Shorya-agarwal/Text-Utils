47fc30bb6ea5462d5ae16872ee9d5ee6fc23bcd9
Shorya-agarwal <91627681+Shorya-agarwal@users.noreply.github.com>
2021-12-12 21:35:49 +0530

    disabling router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import { useState } from 'react';
 import './App.css';
 import Alerts from './components/Alerts';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import{
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+// import{
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
 
 
 function App() {
@@ -42,20 +42,20 @@ function App() {
   }
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" mode={mode} togglemode={togglemode} />
         <Alerts alert={alert} />
         <div className="container my-3">
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
+          {/* <Switch> */}
+            {/* <Route path="/about"> */}
+              {/* <About /> */}
+            {/* </Route> */}
+            {/* <Route path="/"> */}
               <TextForm heading="Enter your text here" mode={mode} showAlert={showAlert} />
-            </Route>
-          </Switch>
+            {/* </Route> */}
+          {/* </Switch> */}
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
